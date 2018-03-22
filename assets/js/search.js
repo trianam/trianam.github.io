8f6ebf63683974f64bc192a6d012de59e2a06322
Stefano Martina <stefano.martina@gmail.com>
2018-03-22 23:22:58 +0100

    adjusted search and post layout

--- assets/js/search.js
+++ assets/js/search.js
@@ -16,7 +16,7 @@ var index = elasticlunr(function () {
 //Add to this index the proper metadata from the Jekyll content
 
 
-{% assign count = 0 %}{% for text in site.texts %}
+{% assign count = 0 %}{% for text in site.documents %}
 index.addDoc({
   title: {{text.title | jsonify}},
   author: {{text.author | jsonify}},
@@ -29,7 +29,7 @@ console.log( jQuery.type(index) );
 // Builds reference data (maybe not necessary for us, to check)
 
 
-var store = [{% for text in site.texts %}{
+var store = [{% for text in site.documents %}{
   "title": {{text.title | jsonify}},
   "author": {{text.author | jsonify}},
   "layout": {{ text.layout | jsonify }},
